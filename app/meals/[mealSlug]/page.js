2774326ce551d7270ff8af81dabf739c231c3bb3
Qamar Abbas <reply.qamar@gmail.com>
2024-08-10 16:29:16 +0500

    meal detail page and meal not found

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,7 +1,32 @@
-export default function MealsPage() {
+import Image from 'next/image';
+import classes from './page.module.css';
+import { getMeal } from '@/lib/meals';
+import { notFound } from 'next/navigation';
+export default function MealsPage({params}) {
+  const meal = getMeal(params?.mealSlug)
+  if(!meal){
+    notFound();
+  }
+  meal.instructions = meal.instructions.replace(/\n/g, '<br>');
   return (
-    <div>
-      <h1>Meals detail Page</h1>
+   <>
+    <header className={classes.header}>
+    <div className={classes.image}>
+      <Image fill src={meal?.image}/>
     </div>
+    <div className={classes.headerText}>
+      <h1>{meal?.title}</h1>
+      <p className={classes.creator}>
+      by <a href={`mailto:${meal.creator_email}`}> {meal?.creator} </a>
+      </p>
+      <p className={classes.summary}>{meal.summary}</p>
+    </div>
+    </header>
+    <main>
+    <p className={classes.instructions} dangerouslySetInnerHTML={{
+      __html: `${meal.instructions}`
+    }} ></p>
+    </main>
+   </>
   );
 }
