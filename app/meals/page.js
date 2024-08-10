157be7b4c9476e6decd049a4734c563cc7c14447
Qamar Abbas <reply.qamar@gmail.com>
2024-08-10 13:01:21 +0500

    added suspense and loading

--- app/meals/page.js
+++ app/meals/page.js
@@ -3,9 +3,14 @@ import Link from 'next/link';
 import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
 
-export default async function MealsPage() {
+export async function Meals (){
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />
+
+}
+export default function MealsPage() {
 
   return (
     <>
@@ -24,7 +29,9 @@ export default async function MealsPage() {
 
       </header>
       <main className={classes.main}>
-      <MealsGrid meals={meals} />
+      <Suspense fallback={<p className={classes.loading}>Featching meals...</p>}>
+        <Meals />
+      </Suspense>
       </main>
     </>
   );
